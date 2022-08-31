bc4ff2c26509926ba1bf80eec1a9caf24e7be5b3
putriharviana <putriharviana@gmail.com>
2022-08-31 17:12:39 +0700

    api completed

--- cypress/e2e/getPostApi.cy.js
+++ cypress/e2e/getPostApi.cy.js
@@ -27,4 +27,48 @@ describe("Get and Post Api", ()=> {
         })
 
     })
+
+    it("Put Api Update User", ()=> {
+        cy.request({
+            method: 'PUT',
+            url: 'https://reqres.in/api/users/2', // baseUrl is prepend to URL
+            form: true, // indicates the body should be form urlencoded and sets Content-Type: application/x-www-form-urlencoded headers
+            body: {
+                "name": "morpheus",
+                "job": "zion resident",
+            },
+        }).then((response)=>{
+            console.log(response)
+            expect(response.status).to.eq(200)
+            expect(response.body).has.property("name", "morpheus")
+            expect(response.body).has.property("job", "zion resident")
+        })
+    })
+
+    it("Patch Api Update User", ()=> {
+        cy.request({
+            method: 'PATCH',
+            url: 'https://reqres.in/api/users/2', // baseUrl is prepend to URL
+            form: true, // indicates the body should be form urlencoded and sets Content-Type: application/x-www-form-urlencoded headers
+            body: {
+                "name": "morpheus",
+                "job": "zion resident",
+            },
+        }).then((response)=>{
+            console.log(response)
+            expect(response.status).to.eq(200)
+            expect(response.body).has.property("name", "morpheus")
+            expect(response.body).has.property("job", "zion resident")
+        })
+    })
+
+    it("Delete Api User", ()=> {
+        cy.request({
+            method: 'DELETE',
+            url: 'https://reqres.in/api/users/2', // baseUrl is prepend to URL
+        }).then((response)=>{
+            console.log(response)
+            expect(response.status).to.eq(204)
+        })
+    })
 })
